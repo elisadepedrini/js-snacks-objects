7205a972c2cb86ed2eadd9c77ae5c934cb5118d2
elisadepedrini <elisa.depedrini@gmail.com>
2026-03-23 18:57:06 +0100

    Complete snack 1

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('ciao');
+
+const bikes = [
+    {
+        name : 'Scott Addict RC Ultimate',
+        weight : 5.9,
+    },
+    {
+        name : 'Giant TCR Advanced SL 0',
+        weight : 6.4,
+    },
+    {
+        name : 'Specialized S-Works Tarmac SL8',
+        weight : 6.8,
+    }
+]
+
+
+let bikeMin = bikes[0]
+
+for (let i = 1; i < bikes.length; i++) {
+    
+    if (bikes[i].weight < bikeMin.weight) {
+        bikeMin = bikes[i]
+    }
+
+}
+
+console.log(bikeMin);
+
+
+const bikeBox = document.getElementById('boxBike')
+
+bikeBox.innerHTML = 'La bici con peso minore è: ' + bikeMin.name + '; con peso: ' + bikeMin.weight
