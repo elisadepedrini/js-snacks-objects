66f003ba5c06fdc9d356ff31ae1f95602bef643f
elisadepedrini <elisa.depedrini@gmail.com>
2026-03-24 11:38:52 +0100

    Create objects's array and generate random number

---
+++ snack-02/main.js
@@ -0,0 +1,26 @@
+const soccerTeams = [
+    {
+        name: 'Milan',
+        goal: 0,
+        foulsWon: 0
+    },
+    {
+        name: 'Inter',
+        goal: 0,
+        foulsWon: 0
+    },
+    {
+        name: 'Juve',
+        goal: 0,
+        foulsWon: 0
+    }
+]
+
+for (let i = 0; i < soccerTeams.length; i++) {
+
+    if (soccerTeams[i].goal === 0 & soccerTeams[i].foulsWon === 0) {
+        soccerTeams[i].goal = Math.floor(Math.random() * 21);
+        soccerTeams[i].foulsWon = Math.floor(Math.random() * 21);
+        console.log(soccerTeams[i]);  
+    } 
+}
